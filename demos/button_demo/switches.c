b13725a19c3473a1ae0e059b8ac50b661232005b
jamachado2 <jamachado2@miners.utep.edu>
2020-03-15 20:56:31 -0600

    update switches.c

--- demos/button_demo/switches.c
+++ demos/button_demo/switches.c
@@ -2,25 +2,26 @@
 #include "switches.h"
 #include "led.h"
 
-char switch_state_down, switch_state_changed; /* effectively boolean */
+char switch_state_down, switch_state_down1, switch_state_changed; /* effectively boolean */
 
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p1val = P2IN; /* P1IN */
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
+  P2IES |= (p1val & SWITCHES);	/* if switch up, sense down */ /* P1IES */
+  P2IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */ /* P1IES */
   return p1val;
 }
 
 void 
 switch_init()			/* setup switch */
-{  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE = SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+{
+  /*  chage bits 0, 1, 2, 3 */
+  P2REN |= SWITCHES;		/* P1REN */ /* enables resistors for switches */
+  P2IE = SWITCHES;		/* P1IE*/ /* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* P1OUT */ /* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* P1DIR */ /* set switches' bits for input */
   switch_update_interrupt_sense();
   led_update();
 }
@@ -29,7 +30,12 @@ void
 switch_interrupt_handler()
 {
   char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  
+  switch_state_down = (p1val & BIT0) ? 0 : 1; /* 0 when SW1 is up */
   switch_state_changed = 1;
+  
+  switch_state_down1 = (p1val & BIT1) ? 0 : 1;
+  switch_state_changed = 1;
+  
   led_update();
 }
