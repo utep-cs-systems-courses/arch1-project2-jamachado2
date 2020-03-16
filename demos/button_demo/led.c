a75e0a7edf64e9fb8baa10a4d6c087e0e4c53ec7
jamachado2 <jamachado2@miners.utep.edu>
2020-03-15 20:55:24 -0600

    update led.c

--- demos/button_demo/led.c
+++ demos/button_demo/led.c
@@ -13,9 +13,16 @@ void led_update(){
   if (switch_state_changed) {
     char ledFlags = 0; /* by default, no LEDs on */
 
-    ledFlags |= switch_state_down ? LED_GREEN : 0;
-    ledFlags |= switch_state_down ? 0 : LED_RED;
-
+    if(switch_state_down){
+      ledFlags |= switch_state_down ? 1 : LED_GREEN;
+      ledFlags |= switch_state_down ? LED_RED : 0;
+    }
+    
+    if(switch_state_down1){
+      ledFlags |= switch_state_down1 ? 0 : LED_RED;
+      ledFlags |= switch_state_down1 ? LED_GREEN : 1; 
+    }
+    
     P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
     P1OUT |= ledFlags;         // set bits for on leds
   }
